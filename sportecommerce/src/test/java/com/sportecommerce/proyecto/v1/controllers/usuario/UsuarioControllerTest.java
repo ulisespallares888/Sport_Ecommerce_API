43f309401013862db15089cb7b767f781a15341d
ulisespallares888 <ulisespallares8@gmail.com>
2025-09-23 20:58:34 -0300

    [FIX] env tests

--- sportecommerce/src/test/java/com/sportecommerce/proyecto/v1/controllers/usuario/UsuarioControllerTest.java
+++ sportecommerce/src/test/java/com/sportecommerce/proyecto/v1/controllers/usuario/UsuarioControllerTest.java
@@ -1,7 +1,11 @@
 package com.sportecommerce.proyecto.v1.controllers.usuario;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@SpringBootTest
+@ActiveProfiles("test")
 class UsuarioControllerTest {
 
     @Test
